bba9c4f0ecd7a9ed9a93654bedd60f562af4753d
Jordi Grant Esteve <jordi.esteve@workday.com>
2018-09-16 15:59:37 +0100

    Initial library
    This is the first patch that contains a functional library.

--- code-for-fun-I/joystick/joystick/joystick.cpp
+++ code-for-fun-I/joystick/joystick/joystick.cpp
@@ -1,76 +1,128 @@
 #include "joystick.h"
 
-Joystick::Joystick(float radius, int mPower) {
-  _radius = radius;
+Joystick::Joystick(int mPower) {
   _mPower = mPower;
 }
 
 void Joystick::setCoordinates(int x, int y) {
-  if (x > 512 && y > 512) {
+  if (x >= 512 && y >= 512) {
     _quadrant = JOYSTICK_FIRST_QUADRANT;
     _x = x - 512;
     _y = y - 512;
-  } else if (x > 512 && y < 512) {
+  } else if (x >= 512 && y < 512) {
     _quadrant = JOYSTICK_SECOND_QUADRANT;
     _x = x - 512;
     _y = 512 - y;
-  } else if (x < 512 && y > 512) {
+  } else if (x < 512 && y < 512) {
     _quadrant = JOYSTICK_THIRD_QUADRANT;
     _x = 512 - x;
     _y = 512 - y;
-  } else {
+  } else if (x < 512 && y >= 512) {
     _quadrant = JOYSTICK_FORTH_QUADRANT;
     _x = 512 - x;
     _y = y - 512;
+  } else {
+    _quadrant = JOYSTICK_AT_CENTRE;
   }
+    
+  float distance = sqrt(_x * _x + _y * _y);
+  _k = _mPower * distance / 512;
 
-  _distance = sqrt(_x * _x + _y * _y);
-
-  _angle = acos(x / _distance);
+  if (distance != 0) {
+    _angle = acos(_x / distance);
+  } else {
+    _angle = 0;
+  }
 }
 
-int Joystick::getLeftMotorPower() {
-  int power;
 
-  switch (_quadrant) {
-  case JOYSTICK_FIRST_QUADRANT:
-    power = (int) (_mPower * _distance / _radius);
-    break;
-  case JOYSTICK_SECOND_QUADRANT:
-    power = (int) (_mPower * _distance / _radius);
-  case JOYSTICK_THIRD_QUADRANT:
-    if (_angle < JOYSTICK_22_5_IN_RAD) {
-      power = (int) (-(_mPower * _distance / _radius) * (_angle - JOYSTICK_22_5_IN_RAD) / JOYSTICK_22_5_IN_RAD);
-    } else if (_angle < JOYSTICK_45_IN_RAD) {
-      power = (int) (-(_mPower * _distance / _radius) * (_angle - JOYSTICK_22_5_IN_RAD) / JOYSTICK_45_IN_RAD);
-    } else {
-      power = (int) (-(_mPower * _distance / _radius) * (JOYSTICK_90_IN_RAD - _angle) / JOYSTICK_90_IN_RAD);
-    }
-    break;
-  case JOYSTICK_FORTH_QUADRANT:      
-    if (_angle < JOYSTICK_22_5_IN_RAD) {
-      power = (int) ((_mPower * _distance / _radius) * (_angle - JOYSTICK_22_5_IN_RAD) / JOYSTICK_22_5_IN_RAD);
-    } else if (_angle < JOYSTICK_45_IN_RAD) {
-      power = (int) ((_mPower * _distance / _radius) * (_angle - JOYSTICK_22_5_IN_RAD) / JOYSTICK_45_IN_RAD);
-    } else {
-      power = (int) (-(_mPower * _distance / _radius) * (JOYSTICK_90_IN_RAD - _angle) / JOYSTICK_90_IN_RAD);
-    }
-    break;
+int Joystick::getLeftMotorPower() {
+  float power;
+
+  switch(_quadrant) {
+    case JOYSTICK_FIRST_QUADRANT:
+      power = 1;
+      break;
+    case JOYSTICK_SECOND_QUADRANT:
+      power = - 1;
+      break;
+
+    case JOYSTICK_THIRD_QUADRANT:
+      if (_angle < JOYSTICK_22_5) {
+        power = - C();
+      } else if (_angle < JOYSTICK_45) {
+        power = - B();
+      } else {
+        power = - A();
+      }
+      break;
+
+    case JOYSTICK_FORTH_QUADRANT:
+      if (_angle < JOYSTICK_22_5) {
+        power = C();
+      } else if (_angle < JOYSTICK_45) {
+        power = B();
+      } else {
+        power = A();
+      }
+      break;
+
+    default:
+      power = 0;
+      break;
   }
 
-  return power;
+  return _k * power;
 }
 
 int Joystick::getRightMotorPower() {
-  int power;
+  float power;
 
   switch(_quadrant) {
-  case JOYSTICK_FIRST_QUADRANT:
-    if (_angle < JOYSTICK_22_5_IN_RAD) {
-      power = (int) ( (_mPower * _distance / _radius) * (JOYSTICK_90_IN_RAD - _angle) / JOYSTICK_90_IN_RAD )
-    } else if (_angle < JOYSTICK_45_IN_RAD) {
-    } else {
-    }
-    break;
+    case JOYSTICK_FIRST_QUADRANT:
+      if (_angle < JOYSTICK_22_5) {
+        power = C();
+      } else if (_angle < JOYSTICK_45) {
+        power = B();
+      } else {
+        power = A();
+      }
+      break;
+    
+    case JOYSTICK_SECOND_QUADRANT:
+      if (_angle < JOYSTICK_22_5) {
+        power = - C();
+      } else if (_angle < JOYSTICK_45) {
+        power = - B();
+      } else {
+        power = - A();
+      }
+      break;
+
+    case JOYSTICK_THIRD_QUADRANT:
+      power = - 1;
+      break;
+
+    case JOYSTICK_FORTH_QUADRANT:
+      power = 1;
+      break;
+
+    default:
+      power = 0;
+      break;
   }
+  return _k * power;
 }
+
+float Joystick::A() {
+  return (1 - (JOYSTICK_90 - _angle) / JOYSTICK_90);
+}
+
+float Joystick::B() {
+  return (_angle - JOYSTICK_22_5) / JOYSTICK_45;
+}
+
+float Joystick::C() {
+  return (_angle - JOYSTICK_22_5) / JOYSTICK_22_5;
+}
+
--- code-for-fun-I/joystick/joystick/joystick.h
+++ code-for-fun-I/joystick/joystick/joystick.h
@@ -8,24 +8,30 @@
 #define JOYSTICK_SECOND_QUADRANT 1
 #define JOYSTICK_THIRD_QUADRANT 2
 #define JOYSTICK_FORTH_QUADRANT 3
-#define JOYSTICK_22_5_IN_RAD 0.3926991f
-#define JOYSTICK_45_IN_RAD 0.785398f
-#define JOYSTICK_90_IN_RAD 1.5708f
+#define JOYSTICK_AT_CENTRE 4
+
+#define JOYSTICK_22_5 0.3926991
+#define JOYSTICK_45 0.785398
+#define JOYSTICK_90 1.5708
 
 class Joystick {
   private:
-  float _radius;
-  int _mPower;
-  int _x;
-  int _y;
+  float _mPower;
+  float _x;
+  float _y;
   int _quadrant;
-  float _distance;
+  float _k;
   float _angle;
 
   public:
-  Joystick(float radius, int mPower);  
+  Joystick(int mPower);  
   void setCoordinates(int x, int y);
   int getLeftMotorPower();
   int getRightMotorPower();
   int getQuadrant();
+
+  private:
+  float A();
+  float B();
+  float C();
 };
